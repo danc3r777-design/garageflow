1a377c21bd9b9255fdb0aebe8c2e455ab85610b4
danc3r777-design <danc3r777@gmail.com>
2026-09-19 14:35:39 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import { createRoot } from "react-dom/client";
 
 import App from "./App.jsx";
 import CrmApp from "./CrmApp.jsx";
+import "./ClientV5.css";
 
 import "./index.css";
 import "./Crm.css";
